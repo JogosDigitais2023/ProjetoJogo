245b6926d89376b7b71684676d3a3b8712f0109f
murillodesu <murillodesu@gmail.com>
2023-08-24 17:43:57 -0300

    build

--- 2d platform test/Assets/Scripts/UIManager.cs
+++ 2d platform test/Assets/Scripts/UIManager.cs
@@ -7,36 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Image imageMage;
-    [SerializeField] private Image imageSlinger;
-    [SerializeField] private Image imageWarrior;
-    [SerializeField] private PlayerController player;
     [SerializeField] private GameObject options;
     [SerializeField] private GameObject pause;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
-
-    private void SetSelecterCharacter(PlayerController player)
-    {
-        // switch (characterState)
-        // {
-        //     case player.CharacterState.mage:
-        //         imageMage.color = new Color(0,1,0);
-        //         imageSlinger.color = new Color(1,1,1);
-        //         imageWarrior.color = new Color(1,1,1);
-        //         break;
-        //     case player.CharacterState.slinger:
-        //         imageMage.color = new Color(1,1,1);
-        //         imageSlinger.color = new Color(0,1,0);
-        //         imageWarrior.color = new Color(1,1,1);
-        //         break;
-        //     case player.CharacterState.warrior:
-        //         imageMage.color = new Color(1,1,1);
-        //         imageSlinger.color = new Color(0,1,0);
-        //         imageWarrior.color = new Color(1,1,1);
-        //         break;
-        // }
-    }
 
     private void Update()
     {
